fdd0e4d69bc25a7631c864b4b7f19b954699dde2
Luis Villeda <luisevilleda@gmail.com>
2017-02-02 12:18:16 -0800

    Update config key explanation

--- src/config.example.js
+++ src/config.example.js
@@ -1,4 +1,7 @@
 export default {
   url: 'https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/findByIngredients',
+  // The API is spoontacular: spoonacular.com/food-api
+    // They make you sign up though market.mashape.com
+    // Once you sign up go to your applications and press 'Get The Keys'
   key: 'KEY_GOES_HERE',
 };
